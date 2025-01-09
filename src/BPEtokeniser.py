fdc83900698b22c7df6f75f9ac49f7c1a45abab5
Zain Sharief <zain.sharief21@gmail.com>
2025-01-09 15:48:42 +0000

    Comments + Made Faster

--- src/BPEtokeniser.py
+++ src/BPEtokeniser.py
@@ -3,48 +3,72 @@ from collections import Counter
 
 class BPETokeniser():
 
-    def __init__(self, num_merges):
+    def __init__(self, num_merges: int) -> None:
         self.encode_map = {}
         self.decode_map = {}
         self.vocab_size = 0
-        self.num_merges = num_merges
-
-    def replace(self, data, max_pair, idx):
+        self.num_merges = num_merges 
+    
+    def replace(self, pairs_list: list[tuple[int]], max_pair: tuple[int], idx: int) -> list[int]:
+        
+        # Creating a new list is faster than inplace operations for large lists
         new_list = []
-        x = 0
-        while x < len(data):
-            if  data[x] == max_pair[0] and x < len(data) - 1 and data[x+1] == max_pair[1]:
+        skip = False
+
+        for item in pairs_list: 
+
+            # If the previous element was the max_pair, skip the current element
+            if skip:
+                skip = False
+
+            elif item == max_pair:
                 new_list.append(idx)
-                x += 2
+                skip = True
+
             else:
-                new_list.append(data[x])
-                x += 1
+                new_list.append(item[0])
+
+        # The last element should be added if the last pair is the max_pair otherwise it is missed
+        if pairs_list[-1] != max_pair:
+            new_list.append(pairs_list[-1][0])
 
         return new_list
 
-    def create_encoding(self, data, min_freq=5, file_path=None):
+    def create_encoding(self, data: str, min_freq: int = 5, file_path: str = None) -> list[int]:
+
+        # Converts the data to utf-8 values
         data = list(map(int, data.encode('utf-8')))
         idx = max(data) + 1
     
         for _ in range(self.num_merges):
+
+            # Has to recalculate the pairs each time to account for new pairs
             pairs_list = list(zip(data[:-1], data[1:]))
             pair_counts = Counter(pairs_list)
+
+            # Validates a pair appears more than min_freq times
             if max(pair_counts.values()) < min_freq:
                 print(f'No pair is present more than {min_freq} times. Terminating...')
                 break
             max_pair = max(pair_counts, key=pair_counts.get)
             
-            data = self.replace(data, max_pair, idx)
+            data = self.replace(pairs_list, max_pair, idx)
             self.encode_map[max_pair] = idx
-            self.decode_map[idx] = max_pair
             idx += 1
 
-        self.vocab_size = max(data) + 1
+        # Decode map is a swapped version of the encode map
+        self.decode_map = {k:v for v,k in self.encode_map.items()}
+
+        # Map only holds the pairs -> not utf-8 values
+        self.vocab_size = max(data) + 1 
+
         if file_path:
             self.save(file_path)
+
         return data
 
-    def encode(self, data):
+    def encode(self, data: str) -> list[int]:
+
         data = list(map(int, data.encode('utf-8')))
 
         for encoding_pair in self.encode_map.keys():
@@ -53,8 +77,10 @@ class BPETokeniser():
         
         return data 
     
-    def decode(self, tokens):
+    def decode(self, tokens: list[int]) -> str:
 
+        # Should replace the tokens in reverse order as this accounts for "nested pairs"
+        # Dictionaries are ordered in python 3.7+ 
         for decode_idx in sorted(self.decode_map.keys(), reverse=True):
             max_pair = self.decode_map[decode_idx]
 
@@ -66,26 +92,47 @@ class BPETokeniser():
 
         return tokens   
 
-    def save(self, file_path):
+    def save(self, file_path: str) -> None:
         with open(file_path, "wb") as f:
-            pickle.dump((self.encode_map, self.decode_map, self.vocab_size), f)
+            # Only need to save the encode map as decode map can be recreated
+            pickle.dump((self.encode_map, self.vocab_size), f)
 
-    def load(self, file_path):
+    def load(self, file_path: str) -> bool:
         try:
             with open(file_path, "rb") as f:
-                self.encode_map, self.decode_map, self.vocab_size = pickle.load(f)
+                self.encode_map, self.vocab_size = pickle.load(f)
+
+                # Recreating the decode map
+                self.decode_map = {k:v for v,k in self.encode_map.items()}
+
             print('Successfully loaded the tokeniser.')
             return True
+        
         except:
             print('Could not load the tokeniser.')
             return False
-        
+
 if __name__ == '__main__':
-    tokeniser_path = 'BPEtokeniser.pkl'
 
-    tokeniser = BPETokeniser(num_merges=10_000)
-    tokeniser.load(tokeniser_path)
+    import timeit
 
-    print(tokeniser.encode('You mean I am groaned a few words?'))
-    print(tokeniser.encode("You are right. Our company’s Christmas party has always been an amazing occasion for everybody."))
+    # Wrapper function for timing
+    def time_create_encoding(tokeniser_class, data):
+        tokeniser = tokeniser_class(1000)
+        tokeniser.create_encoding(data)
 
+    # The data for testing
+    data = """2MGKwAS1PzI4z6hs1kuDX9OYIwKhkJPG7a5LG3CqZnQjbpCanhqeAClXv5HtuNfIRQ48qZNQWAL41R7PR8h5oh5QAEeIRRdIb7rHmAVO0DlEQjBhgTcRCsCzQ4F18PcMypsCntbzjN0UuUklUyONM7NNeqiaqsYEq0DDvwrZCBvlfTo5QqVm52yuihh1Ti4dSRAnrrDo
+    Rh3A4Teu3pz6NYwJFd2FbX9fDjD12Yi9CDM5MIHK5R01pwlQCiW4iZWfA4BZc1OLyQpnUZYrkARN6AVT6n6Zp4IAjbMn2o1OGUP6Zg0L1vJuRUdIIAdqeji322YQjyTDyKbcAkzxpncFhYPWvRuw44TKQPGV3AGoXkoAz 94x04sq1zq6GvST7Bva38NViHmeJZocbtT
+    Vdal5TNnF7F s5tB4AOzosAN7lsWPlMolMTLUG53EluCwyFMvSXUtcaXq0GuuZnzsS6NZLtaxIp1e4RzeIapAd7lJImSWNYVceB6bvqtjBMRXeypbzNImSEBAo8nD9W fjXs6Q7roclOdNc6PS1WiHkpsirF1e48mLyYM3hgmMEsIKU3OQJjA0JSsG pWagL9Xjl PVP
+    6OWctzzcqrSY1d2dSxejZ0G3Vwuiao0KkQL1d6BYeTBrPxqsEavDLKuMQBLD4TpzXFBQRQ81YLFwklR91juhZ1YoVo7FWnNvtQJvTT3JH31 S1YIVBwqaSrC6C5jXQhh6 k9nnlYgw7kaD2R9wjAA4zJIAnNnDVg2vVoMbpZ3eJSDEoWtq6aSABGu96NKs3QuYX6N88F
+    DlsxbTXv020znL1qTGfaVuDNQ3yZHLWm2ljJu4lg280PLQ2BKuOINFw 1Lb7a1a7Z1g1sDxswlli5OIrlzWlFAl2KJNhxkjJREcUINsYghbX9pCbFXbgHihZTcITe9F918qw8xTXKXJvj4h3neBSePP7eekY0o N1SZPIHN2yJsAGgAAorxZGdGB2oXXYTUsbic47ad8"""
+
+    # Timing BPETokeniser
+    time_tokeniser = timeit.timeit(
+        stmt="time_create_encoding(BPETokeniser, data)",
+        globals=globals(),
+        number=10 
+    )
+
+    print(f"BPETokeniser time: {time_tokeniser:.6f} seconds (average over 10 runs)")
