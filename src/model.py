430be142434280a352a10174e6f2a9b2bb81b16f
Zain Sharief <zain.sharief21@gmail.com>
2025-02-12 23:21:33 +0000

    Added RotaryPositionalEmbedding and RMSNorm

--- src/model.py
+++ src/model.py
@@ -12,7 +12,7 @@ class GPTModel(nn.Module):
         self.device = device
 
         self.embedding = Embedding(vocab_size, embedding_dim, context_size, dropout)
-        self.blocks = nn.Sequential(*[Block(embedding_dim, num_heads, device, projection, dropout) for _ in range(num_layers)])
+        self.blocks = nn.Sequential(*[Block(embedding_dim, num_heads, context_size, device, projection, dropout) for _ in range(num_layers)])
         self.out_projection = nn.Linear(embedding_dim, vocab_size, bias=False)
 
         self.apply(self._init_weights)
@@ -32,8 +32,10 @@ class GPTModel(nn.Module):
     @torch.no_grad()
     def generate(self, tokeniser, text, temperature, k, max_new_tokens):
 
+        training_mode = self.training 
+        self.eval()
+
         # Encodes the text and adjusts size to context_size
-        text = tokeniser.eos_token + text
         context = tokeniser.encode(text)[-self.context_size:].to(self.device)
 
         output = ''
@@ -55,10 +57,8 @@ class GPTModel(nn.Module):
             idx = idx[0].int()
             output += tokeniser.decode(idx)
 
-            # Returns the sequence if the end of sequence is reached
-            if tokeniser.eos_token in output:
-                return output.split(tokeniser.eos_token)[0]
-
+        self.train(training_mode)
+            
         return output
 
 class Embedding(nn.Module):
@@ -67,51 +67,32 @@ class Embedding(nn.Module):
         super().__init__()
         self.context_size = context_size
         self.token_embedding = nn.Embedding(vocab_size, embedding_dim)
-        self.positional_encoding = PositionalEncoding(context_size, embedding_dim)
         self.dropout = nn.Dropout(p=dropout)
 
     def forward(self, x):
         x = self.token_embedding(x)
-        x = self.positional_encoding(x)
         return self.dropout(x)
-
-class PositionalEncoding(nn.Module):
-
-    def __init__(self, context_size, d_model):
-        super(PositionalEncoding, self).__init__()
-
-        pe = torch.zeros(context_size, d_model)
-        position = torch.arange(context_size, dtype=torch.float).unsqueeze(1)
-        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
-        pe[:, 0::2] = torch.sin(position * div_term)
-        pe[:, 1::2] = torch.cos(position * div_term)
-        pe = pe.unsqueeze(0)
-        self.register_buffer('pe', pe)
-
-    def forward(self, x):
-        x += self.pe[:, :x.size(1), :]
-        return x
 
 class Block(nn.Module):
 
-    def __init__(self, embedding_dim, num_heads, device, projection=4, dropout=0.1):
+    def __init__(self, embedding_dim, num_heads, context_size, device, projection=4, dropout=0.1):
         super().__init__()
-        self.layernorm1 = nn.LayerNorm(embedding_dim)
-        self.multiheadattention = MultiHeadAttention(embedding_dim, num_heads, dropout, device)
+        self.rmsnorm1 = nn.RMSNorm(embedding_dim)
+        self.multiheadattention = MultiHeadAttention(embedding_dim, num_heads, context_size, dropout, device)
         self.dropout1 = nn.Dropout(dropout)
 
-        self.layernorm2 = nn.LayerNorm(embedding_dim)
+        self.rmsnorm2 = nn.RMSNorm(embedding_dim)
         self.feedforward = FeedForward(embedding_dim, projection)
         self.dropout2 = nn.Dropout(dropout)
 
     def forward(self, x):
-        x = x + self.dropout1(self.multiheadattention(self.layernorm1(x)))
-        x = x + self.dropout2(self.feedforward(self.layernorm2(x)))
+        x = x + self.dropout1(self.multiheadattention(self.rmsnorm1(x)))
+        x = x + self.dropout2(self.feedforward(self.rmsnorm2(x)))
         return x
-    
+
 class MultiHeadAttention(nn.Module):
 
-    def __init__(self, embedding_dim, num_heads, dropout, device):
+    def __init__(self, embedding_dim, num_heads, context_size, dropout, device):
         super().__init__()
         self.device = device
 
@@ -125,21 +106,27 @@ class MultiHeadAttention(nn.Module):
         self.proj = nn.Linear(self.embedding_dim, self.embedding_dim)
         self.dropout = nn.Dropout(dropout)
 
+        mask = torch.tril(torch.ones(context_size, context_size, device=self.device, dtype=torch.long)).unsqueeze(0).unsqueeze(1)
+        self.register_buffer("mask", mask)
+
+        self.rope = RotaryPositionalEmbedding(embedding_dim, context_size)
+
     def forward(self, x):
         B, T, C = x.size()
-
-        mask = torch.tril(torch.ones(T, T, device=self.device, dtype=torch.long)).view(1, 1, T, T)
 
         qkv = self.attn(x)
         q, k, v = qkv.chunk(3, dim=2)
 
+        q = self.rope(q)
+        k = self.rope(k)
+
         # (B, num_heads, T, head_size)
-        k = k.view(B, T, self.num_heads, self.head_size).transpose(1, 2)
         q = q.view(B, T, self.num_heads, self.head_size).transpose(1, 2)
+        k = k.view(B, T, self.num_heads, self.head_size).transpose(1, 2)
         v = v.view(B, T, self.num_heads, self.head_size).transpose(1, 2)
 
         wei = (q @ k.transpose(-2, -1)) * (1.0 / math.sqrt(self.head_size))
-        wei = wei.masked_fill(mask[:, :, :T, :T] == 0, float('-inf'))
+        wei = wei.masked_fill(self.mask[:, :, :T, :T] == 0, float('-inf'))
 
         wei = F.softmax(wei, dim=-1)
         wei = self.dropout(wei)
@@ -149,6 +136,26 @@ class MultiHeadAttention(nn.Module):
 
         x = self.proj(x)
         return x
+
+class RotaryPositionalEmbedding(nn.Module):
+
+    def __init__(self, dim, max_seq_len):
+        super(RotaryPositionalEmbedding, self).__init__()
+
+        theta = (10_000 ** ((-2 * torch.arange(0, dim, 2, dtype=torch.float)) / dim)).unsqueeze(0)
+        position = torch.arange(max_seq_len, dtype=torch.float).unsqueeze(1)
+        angles = position * theta
+
+        self.register_buffer("cos", torch.cos(angles).unsqueeze(0).unsqueeze(-1))
+        self.register_buffer("sin", torch.sin(angles).unsqueeze(0).unsqueeze(-1))
+
+    def forward(self, x):
+        B, T, C = x.size()
+        x = x.view(B, T, C // 2, 2) 
+        x_rot = torch.cat([-x[..., 1:2], x[..., 0:1]], dim=-1)
+
+        x_out = x * self.cos[:, :T] + x_rot * self.sin[:, :T]
+        return x_out.view(B, T, C)
 
 class FeedForward(nn.Module):
 
