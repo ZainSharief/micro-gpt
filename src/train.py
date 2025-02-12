2b84e316ceca295917a430e70b9fec0fbf612f3f
Zain Sharief <zain.sharief21@gmail.com>
2025-02-12 23:23:16 +0000

    Hyperparameter Update

--- src/train.py
+++ src/train.py
@@ -24,65 +24,76 @@ if torch.cuda.is_available():
 
 @dataclass
 class config:
-    embedding_dim: int = 768
+    embedding_dim: int = 1024
     context_size: int = 256
-    num_heads: int = 12
+    num_heads: int = 16
     num_layers: int = 12
+    max_norm: float = 1.0
 
-batch_size = 16                         # Number of samples in each batch (16 to prevent CUDA memory errors)
-learning_rate = 2e-4
-max_lr = 6e-4
-inference_iter = 100                    # Number of iterations before inference
-save_iter = 100_000                     # Number of iterations before saving the model
-
-# Load the dataset & tokeniser
-tokeniser = GPTtokenizer()
-dataset = FineWeb(tokeniser=tokeniser, context_size=config.context_size, batch_size=batch_size, device=device) 
-total_steps = len(dataset)
-
-model = GPTModel(tokeniser.vocab_size, config.embedding_dim, config.context_size, config.num_heads, config.num_layers, device, dropout=0.2)
-optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
-scheduler = torch.optim.lr_scheduler.OneCycleLR(
-    optimizer, 
-    max_lr=max_lr,
-    total_steps=total_steps,
-    pct_start=0.3, 
-    anneal_strategy='cos'
-)
-scaler = torch.amp.GradScaler(device) 
-m = model.to(device)
-
-for current_step in range(total_steps):
-
-    start_time = time.time()
-
-    # Collect the sample of data for that batch
-    xb, yb = dataset.__getbatch__(current_step)
-
-    optimizer.zero_grad()
-
-    with torch.amp.autocast(device, dtype=torch.float16):
-        out = model(xb)
-        loss = calculate_loss(out, yb)
-    scaler.scale(loss).backward()
-
-    scaler.step(optimizer)
-    scheduler.step()
-    scaler.update()
-
-    total_time = time.time() - start_time
+def main():
     
-    print(f"batch: {current_step+1}/{total_steps} | loss: {loss:.4f} | lr: {scheduler.get_last_lr()[0]:.4e} | step_time: {int(total_time*1000)}ms") 
+    batch_size = 8                          # Number of samples in each batch (16 to prevent CUDA memory errors)
+    learning_rate = 2e-4
+    max_lr = 4e-4
+    inference_iter = 10_000                 # Number of iterations before inference
+    save_iter = 100_000                     # Number of iterations before saving the model
 
-    if not (current_step + 1) % inference_iter:
-        print(model.generate(tokeniser, '', temperature=0.7, k=20, max_new_tokens=100))
+    # Load the dataset & tokeniser
+    tokeniser = GPTtokenizer()
+    dataset = FineWeb(tokeniser=tokeniser, context_size=config.context_size, batch_size=batch_size, device=device) 
+    total_steps = 200_000 #len(dataset)
 
-    if not (current_step + 1) % save_iter:
+    model = GPTModel(tokeniser.vocab_size, config.embedding_dim, config.context_size, config.num_heads, config.num_layers, device, dropout=0.2)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer, 
+        max_lr=max_lr,
+        total_steps=total_steps,
+        pct_start=0.3, 
+        anneal_strategy='cos'
+    )
+    scaler = torch.amp.GradScaler(device) 
+    model = model.to(device)
 
-        checkpoint = {
-            'model_state_dict': model.state_dict(),
-            'optimizer_state_dict': optimizer.state_dict(),                
-            'scheduler_state_dict': scheduler.state_dict(),
-            'scaler_state_dict': scaler.state_dict()
-        }
-        torch.save(checkpoint, 'model_checkpoint.pth')
+    for current_step in range(total_steps):
+
+        start_time = time.time()
+
+        # Collect the sample of data for that batch
+        xb, yb = dataset.__getbatch__(current_step)
+
+        optimizer.zero_grad()
+
+        with torch.amp.autocast(device, dtype=torch.float16):
+            out = model(xb)
+            loss = calculate_loss(out, yb)
+        scaler.scale(loss).backward()
+
+        scaler.unscale_(optimizer)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), config.max_norm)
+
+        scaler.step(optimizer)
+        scheduler.step()
+        scaler.update()
+
+        total_time = time.time() - start_time
+        
+        print(f"\rbatch: {current_step+1}/{total_steps} | loss: {loss:.4f} | lr: {scheduler.get_last_lr()[0]:.4e} | step_time: {int(total_time*1000)}ms", end='') 
+
+        if not (current_step + 1) % inference_iter:
+            print('\n' + model.generate(tokeniser, '', temperature=0.7, k=20, max_new_tokens=100))
+
+        if not (current_step + 1) % save_iter:
+
+            checkpoint = {
+                'model_state_dict': model.state_dict(),
+                'optimizer_state_dict': optimizer.state_dict(),                
+                'scheduler_state_dict': scheduler.state_dict(),
+                'scaler_state_dict': scaler.state_dict()
+            }
+            torch.save(checkpoint, f'model_checkpoint{current_step+1}.pth')
+
+    torch.save(model.state_dict(), 'model_final.pth')
+
+if __name__ == '__main__':
+    main()
