305164219f16ab7d248e0a670724e59241de8f4f
Zain Sharief <zain.sharief21@gmail.com>
2025-09-17 02:47:08 +0100

    Fine-Tune Model Testing + Inference

---
+++ inference/fine_tune_model.py
@@ -0,0 +1,34 @@
+import torch 
+
+from microgpt.tokenizer import GPTtokenizer
+from microgpt.model.model import GPTModel
+from microgpt.config import Config
+
+def main():
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    config = Config()
+    tokenizer = GPTtokenizer()
+    model = GPTModel(config, use_lora=True).to(device)
+    checkpoint = torch.load('weights/fine_tuned_checkpoint_8.pth', weights_only=True)
+    model.load_state_dict(checkpoint['model_state_dict'], strict=True) 
+    model.eval()
+
+    conversation = ''
+    with torch.no_grad():
+
+        while True:
+
+            conversation += tokenizer.user_token
+            prompt = input("query: ")
+            conversation += prompt + tokenizer.end_user_token
+
+            conversation += tokenizer.assistant_token
+            output = model.generate(tokenizer, conversation, temperature=config.temperature, k=config.k, max_new_tokens=100, device=device)
+            conversation += output + tokenizer.end_assistant_token
+
+            print('assisstant: ' + output)
+   
+if __name__ == '__main__':
+    main()
