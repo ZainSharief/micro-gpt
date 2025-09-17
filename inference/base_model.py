be1a501367f54d94e9596a4ed7fb9e057e520501
Zain Sharief <zain.sharief21@gmail.com>
2025-09-17 01:54:32 +0100

    Base Model Testing + Inference

---
+++ inference/base_model.py
@@ -0,0 +1,25 @@
+import torch 
+
+from microgpt.tokenizer import GPTtokenizer
+from microgpt.model.model import GPTModel
+from microgpt.config import Config
+
+def main():
+
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+    config = Config()
+    tokenizer = GPTtokenizer()
+    model = GPTModel(config, use_lora=False).to(device)
+    checkpoint = torch.load('weights/base_model_checkpoint_190000.pth', map_location=device)
+    model.load_state_dict(checkpoint['model_state_dict'], strict=True)
+    model.eval()
+
+    with torch.no_grad():
+
+        while True:
+            prompt = input("query: ")
+            print(model.generate(tokenizer, prompt, temperature=config.temperature, k=config.k, max_new_tokens=100, device=device))
+   
+if __name__ == '__main__':
+    main()
--- tests/model_test.py
+++ tests/model_test.py
@@ -1,35 +1,90 @@
-import torch 
+import torch
+from torch.utils.data import DataLoader
+import time
 
 from microgpt.tokenizer import GPTtokenizer
-from microgpt.model.model import GPTModel
 from microgpt.config import Config
+from microgpt.model.model import GPTModel
+
+'''
+Testing the architecture of the base model.
+
+If the base model is working, it should successfully overfit the dataset
+and the loss will converge to 0.
+'''
+
+class DummyDataset(torch.utils.data.Dataset):
+
+    def __init__(self, tokenizer: GPTtokenizer, context_size: int = 384, device: str = 'cpu'):
+
+        self.tokenizer = tokenizer
+        self.context_size = context_size
+        self.device = device
+
+        text = 'The capital of France is Paris.'
+        self.data = self.tokenizer.encode(text).to(device).squeeze(0)
+
+        if self.data.size(-1) > self.context_size:
+            self.data = self.data[:self.context_size]
+        
+        elif self.data.size(-1) < self.context_size:
+            pad_size = self.context_size - self.data.size(-1)
+            self.data = torch.concat([self.data, torch.zeros(pad_size + 1, dtype=torch.long, device=device)], dim=-1)
+    
+    def __len__(self):
+        return 1_000_000
+
+    def __getitem__(self, _):  
+        return self.data[:-1], self.data[1:]
+    
+    def get_first_token(self):
+        return self.tokenizer.decode(self.data[0])
 
 def main():
 
-    device = 'cpu'
-    torch.manual_seed(411)
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-    if torch.mps.is_available():
-        device = 'mps'
-        torch.mps.manual_seed(411)
-
-    if torch.cuda.is_available():
-        device = 'cuda'
-        torch.cuda.manual_seed(411)
+    # dummy trainig params
+    batch_size = 32                      
+    learning_rate = 4e-4
+    max_lr = 8e-4
 
     config = Config()
-    tokeniser = GPTtokenizer()
-    model = GPTModel(config)
-    m = model.to(device)
+    tokenizer = GPTtokenizer()
+    dataset = DummyDataset(tokenizer=tokenizer, context_size=config.context_size, device=device) 
+    dataloader = DataLoader(dataset, batch_size=batch_size)
 
-    final_weights = torch.load(config.base_model_path, map_location=device)
-    model.load_state_dict(final_weights['model_state_dict'], strict=True)
-    model.eval()
+    model = GPTModel(config, use_lora=False)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate, weight_decay=0.01)
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(
+        optimizer, 
+        max_lr=max_lr,
+        total_steps=1_000_000,
+        pct_start=0.1, 
+        anneal_strategy='cos'
+    )
+    scaler = torch.amp.GradScaler(device)
+    model = model.to(device)
+    
+    for current_batch, (xb, yb) in enumerate(dataloader):
 
-    with torch.no_grad():
-        prompt = input("Query: ")
-        if prompt != '':
-            print(model.generate(tokeniser, prompt, temperature=config.temperature, k=config.k, max_new_tokens=100, device=device))
-   
+        start_time = time.time()
+        optimizer.zero_grad(set_to_none=True)
+
+        with torch.autocast(device_type=device, dtype=torch.float16):
+            _, loss = model(xb, yb)
+        scaler.scale(loss).backward()
+
+        scaler.unscale_(optimizer)
+        torch.nn.utils.clip_grad_norm_(model.parameters(), config.max_norm)
+
+        scaler.step(optimizer)
+        scaler.update()
+        scheduler.step()
+
+        total_time = time.time() - start_time
+    
+        print(f"\rbatch: {current_batch+1}/{len(dataset)} | loss: {loss:.4f} | lr: {scheduler.get_last_lr()[0]:.4e} | step_time: {int(total_time*1000)}ms", end='') 
+        
 if __name__ == '__main__':
     main()
