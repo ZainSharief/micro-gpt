079c6fd617db340e2e0e8f2e6dced83b2b77736a
Zain Sharief <zain.sharief21@gmail.com>
2026-02-17 14:14:43 +0000

    updated finetuning arguments

--- training/finetune.sh
+++ training/finetune.sh
@@ -5,11 +5,11 @@ python -u microgpt/train.py \
     --model_load_path model/pretrain_model.pth \
     --seed 42 \
     --epochs 8 \
-    --batch_size 16 \
+    --batch_size 128 \
     --batch_acc_size 8 \
     --weight_decay 0.1 \
     --lr 1e-4 \
     --max_lr 2e-4 \
-    --validaton_iter 50 \
+    --validaton_iter 100 \
     --checkpoint_path weights/finetune_checkpoint.pth \
     --final_path weights/fine_tuned_model.pth
