49ce032707c5caae44ca4176c21a501c8d9916a0
Zain Sharief <zain.sharief21@gmail.com>
2026-05-21 22:36:00 +0100

    tune finetuning hyperparameters

--- training/finetune.sh
+++ training/finetune.sh
@@ -4,12 +4,14 @@ python -u microgpt/train.py \
     --dropout 0.05 \
     --model_load_path model/pretrain_model.pth \
     --seed 42 \
-    --epochs 3 \
-    --batch_size 128 \
+    --epochs 5 \
+    --batch_size 16 \
     --batch_acc_size 8 \
-    --weight_decay 0.1 \
-    --lr 1e-4 \
-    --max_lr 2e-4 \
+    --weight_decay 0.01 \
+    --lr 1e-5 \
+    --embedding_lr 1e-5 \
+    --max_lr 5e-5 \
+    --embedding_max_lr 5e-5 \
     --validation_iter 100 \
     --checkpoint_path weights/finetune_checkpoint.pth \
     --final_path weights/fine_tuned_model.pth
